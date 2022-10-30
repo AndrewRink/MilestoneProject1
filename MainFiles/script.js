391dcd8cff23dde5bd59f0f3e3558b1db190a541
Andrew <andrewcrink@gmail.com>
2022-10-30 14:16:26 -0400

    added category columns, column test css, and began js

--- MainFiles/script.js
+++ MainFiles/script.js
@@ -2,7 +2,8 @@
 class runGame {
     constructor (element, options ={}) {
 
-    }
+        this.useCategoryIds=options.useCategoryIds || [14, 2929, 67, 10, 18280]}
+
 }
 
 const game = runGame(document.querySelector('.app'), {});
